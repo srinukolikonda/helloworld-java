83ba9a08c061f440698a5600832c306f11b2c16a
srinukolikonda <38029503+srinukolikonda@users.noreply.github.com>
2018-04-24 15:01:40 +0530

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
+}
+
+maven
+
+its a one of the java project type
+
+https://github.com/unixnaresh/devops-maven-example.git
